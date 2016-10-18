e28976030d1f3862e59b065316b3ed960a6bfb5a
Vasco Silva <vasco.s.rebolo@gmail.com>
2016-10-18 16:26:02 +0100

    Change readme, Add directive for recipes (ngFor)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,8 +6,11 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
-  recipe = new Recipe('Dummy', 'Dummy', 'http://www.pollosdumy.com/ribbon.png');
+  recipes: Recipe[] = [
+    new Recipe('Bacalhau à Brás', 'Very very good', 'https://www.pingodoce.pt/wp-content/uploads/2016/03/617x370_bacalhau.jpg', []),
+    new Recipe('Francesinha', 'Awesome', 'https://www.esnporto.org/sites/default/files/events/images/francesinha3.jpg', [])
+  ];
+  // recipe = new Recipe('Dummy', 'Dummy', 'http://www.pollosdumy.com/ribbon.png');
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
   constructor() { }
--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -1,3 +1,10 @@
+import {Ingredient} from '../shared/ingredient';
+
 export class Recipe {
-  constructor(public name, public description, public imagePath) { }
+  constructor(
+    public name,
+    public description,
+    public imagePath,
+    public ingredients: Ingredient[]
+  ) { }
 }
