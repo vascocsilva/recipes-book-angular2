788bcbaed4bf88dae5b9d91a781b8039f4477a32
Vasco Silva <vasco.s.rebolo@gmail.com>
2016-10-21 10:34:18 +0100

    Add routes, add routing no app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.comp
 import { DropdownDirective } from './dropdown.directive';
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { routing } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [
     RecipeService,
